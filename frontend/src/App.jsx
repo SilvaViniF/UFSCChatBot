3da4030e31b899ea747adc6d45771f1e98281e36
Vitor Torii <vitortorii@gmail.com>
2024-04-28 08:24:48 -0300

    feat: Configs/Header

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { ChatUfscPage } from "./pages/ChatUfscPage";
+import { GlobalStyle } from "./assets/styles/reset";
+
+function App() {
+  return (
+    <div>
+      <GlobalStyle />
+      <ChatUfscPage />
+    </div>
+  );
+}
+
+export default App;
